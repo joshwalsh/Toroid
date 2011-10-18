62d52756b32066a72956eb8a30f8aabf60b3fdd3
Josh Walsh <jwalsh@designinginteractive.com>
2011-10-17 23:48:52 -0400

    Configurable terminal runner

--- bin/runner.rb
+++ bin/runner.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 $: << File.dirname(__FILE__) + "/../lib"
 
 require 'game'
@@ -6,20 +8,31 @@ require 'planet_populator'
 require 'planet_renderer'
 require 'console_outputter'
 
-10.times do |i|
-  session = Game.new 10,10
-  session.populate 15
+ConsoleOutputter.output "Board width: "
+width = gets.to_i
+ConsoleOutputter.output "Board height: "
+height = gets.to_i
+ConsoleOutputter.output "Population: "
+population = gets.to_i
+ConsoleOutputter.output "Years: "
+years = gets.to_i
+
+ConsoleOutputter.new_line
+
+years.times do |i|
+  session = Game.new width,height
+  session.populate population
 
   render = PlanetRenderer.new session.planet
 
   ConsoleOutputter.output render.render
   ConsoleOutputter.new_line
-  ConsoleOutputter.output "ITERATION #{i}"
+  ConsoleOutputter.output "#{i + 1} YEARS OLD"
 
   ConsoleOutputter.flush
 
-  ConsoleOutputter.move_cursor_up(10)
+  ConsoleOutputter.move_cursor_up(height)
   sleep 1
 end
 
-ConsoleOutputter.move_cursor_down(10)
+ConsoleOutputter.move_cursor_down(height)
