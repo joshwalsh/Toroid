0f95b1e988f302fca05981ab5fc90cca6a874bf3
Josh Walsh <jwalsh@designinginteractive.com>
2011-10-24 21:36:39 -0400

    Refactor planet renderer specs

--- spec/planet_renderer_spec.rb
+++ spec/planet_renderer_spec.rb
@@ -1,25 +1,33 @@
 require 'spec_helper'
 
 describe Tordoid::PlanetRenderer do
-  before(:each) do
-    @planet = Tordoid::Planet.new 3,3
-    @renderer = Tordoid::PlanetRenderer.new @planet
-  end
+  let(:renderer) { Tordoid::PlanetRenderer.new planet }
 
-  context "ASCII" do
-    it "renders a row" do
-      @planet[2,2] = 'x'
-      @renderer.render_row(2).should == '. . x'
-
-      @planet[1,2] = 'x'
-      @renderer.render_row(2).should == '. x x'
+  context "rendering a single row" do
+    context "one column" do
+      let(:planet) { stub row: [nil, nil, 'x'] }
+      it "renders a row" do
+        renderer.render_row(2).should == '. . x'
+      end
     end
 
-    it "renders a block" do
-      @planet[2,2] = 'x'
-      @planet[1,2] = 'x'
+    context "two columns" do
+      let(:planet) { stub row: [nil, 'x', 'x'] }
+      it "renders a row" do
+        renderer.render_row(2).should == '. x x'
+      end
+    end
+  end
 
-      @renderer.render.should == ". . .\n. . .\n. x x"
+  context "rendering a planet" do
+    let(:planet) { stub height: 3 }
+
+    it "renders a block" do
+      planet.should_receive(:row).with(0).and_return [nil, nil, nil]
+      planet.should_receive(:row).with(1).and_return [nil, nil, nil]
+      planet.should_receive(:row).with(2).and_return [nil, 'x', 'x']
+
+      renderer.render.should == ". . .\n. . .\n. x x"
     end
   end
 end
