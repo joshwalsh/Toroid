62e817533d03143665ccdb1fc3d9b40ea37cc2c2
Josh Walsh <jwalsh@designinginteractive.com>
2011-10-26 21:19:16 -0400

    refactored planet spec

--- spec/planet_spec.rb
+++ spec/planet_spec.rb
@@ -1,84 +1,80 @@
 require 'spec_helper'
 
 describe Tordoid::Planet do
-  before (:each) do
-    @planet = Tordoid::Planet.new(10,10)
-  end
+  subject { Tordoid::Planet.new(10,10) }
 
   context 'is empty' do
-    it 'starts with an empty grid' do
-      @planet.should be_empty
-    end
+    it { should be_empty }
 
     it 'has a 10 x 10 grid' do
-      @planet.size.should == 100;
+      subject.size.should == 100;
     end
   end
 
   context 'manipulates the grid' do
     it 'reads empty grid cells' do
-      @planet[3,6].should be_nil
+      subject[3,6].should be_nil
     end
 
     it 'sets a value to a cell' do
-      @planet[3,6] = 'x'
-      @planet[3,6].should == 'x'
+      subject[3,6] = 'x'
+      subject[3,6].should == 'x'
     end
 
     it 'does not allow setting an organism outside the grid' do
       lambda {
-        @planet[14,18] = 'x'
+        subject[14,18] = 'x'
       }.should raise_error(Tordoid::Planet::OutOfBoundsError)
     end
 
     it 'does not allow reading outside the grid' do
       lambda {
-        @planet[14,18]
+        subject[14,18]
       }.should raise_error(Tordoid::Planet::OutOfBoundsError)
     end
 
     it 'removes an organism from the grid' do
-      @planet[3,6] = 'x'
-      @planet[3,6] = nil
+      subject[3,6] = 'x'
+      subject[3,6] = nil
       
-      @planet.should be_empty
+      subject.should be_empty
     end
   end
 
   context "iterates itself" do
     it 'lists the cells in a row' do
-      @planet[0,2] = 'x'
-      @planet.row(2).should == ['x',nil,nil,nil,nil,nil,nil,nil,nil,nil]
+      subject[0,2] = 'x'
+      subject.row(2).should == ['x',nil,nil,nil,nil,nil,nil,nil,nil,nil]
     end
     
     it "lists the cells in a column" do
-      @planet[2,0] = 'x'
-      @planet.column(2).should == ['x',nil,nil,nil,nil,nil,nil,nil,nil,nil]
+      subject[2,0] = 'x'
+      subject.column(2).should == ['x',nil,nil,nil,nil,nil,nil,nil,nil,nil]
     end 
   end
 
   context "understands the population" do
     it "lists the occupied cells" do
-      @planet[0,1] = 'x'
-      @planet[0,2] = 'x'
+      subject[0,1] = 'x'
+      subject[0,2] = 'x'
 
-      @planet.occupied_cells.should == {[0,1] => 'x', [0,2] => 'x'}
+      subject.occupied_cells.should == {[0,1] => 'x', [0,2] => 'x'}
     end
 
     it "counts occupied cells" do
-      @planet[0,1] = 'x'
-      @planet[0,2] = 'x'
-      @planet[1,1] = 'x'
-      @planet[2,2] = 'x'
+      subject[0,1] = 'x'
+      subject[0,2] = 'x'
+      subject[1,1] = 'x'
+      subject[2,2] = 'x'
 
-      @planet.occupied_cells.count.should == 4
+      subject.occupied_cells.count.should == 4
     end
   end
 
   context "generations" do
     it "ages over time" do
-      @planet.age 3
-      @planet.generation.should == 3
+      subject.age 3
+      subject.generation.should == 3
     end
   end
 end
