7df8fc77f7dda206dd81f151753cfb5a91df5843
Josh Walsh <jwalsh@designinginteractive.com>
2011-10-26 20:32:47 -0400

    Refactor game spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,20 +1,19 @@
 require 'spec_helper'
 
 describe Tordoid::Game do
+  subject { Tordoid::Game.new 10, 10 }
+
   context 'new game' do
     it "creates a planet" do
-      @game = Tordoid::Game.new(10,10)
-
-      @game.planet.size.should == 100 
+      subject.planet.size.should == 100 
     end
   end
 
   context 'game runs' do
     it "coordinates tasks for each run" do
-      @game = Tordoid::Game.new(10,10)
-      @game.planet = mock('planet')
-      @game.planet.should_receive(:age).exactly(3).times
-      @game.run 3
+      subject.planet = mock('planet')
+      subject.planet.should_receive(:age).exactly(3).times
+      subject.run 3
     end
   end
 end
