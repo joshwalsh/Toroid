556289a713bc49e767f68a1fa654cb9008d75de0
Jonathan Penn <jonathan@navel-labs.com>
2011-10-31 23:24:16 -0400

    Game now lets you inject new cell navigators and planet populators.
    But it also has "defaults" that will be instantiated like before if you
    don't override them using the setter methods.

--- lib/tordoid/game.rb
+++ lib/tordoid/game.rb
@@ -1,6 +1,7 @@
 module Tordoid
   class Game
     attr_accessor :planet
+    attr_accessor :cell_navigator, :planet_populator
 
     def initialize(x = 10, y = 10, population = 0)
       @planet = Planet.new x,y
@@ -14,11 +15,23 @@ module Tordoid
       end
     end
 
+    # Treat these two method definitions as "convenience"
+    # initializers to give you the two collaborators
+    # you were instantiating inline. You can now inject
+    # custom or stubbed versions of these instances in
+    # your test code.
+    def cell_navigator
+      @cell_navigator ||= CellNavigator.new @planet
+    end
+
+    def planet_populator
+      @planet_populator ||= PlanetPopulator.new
+    end
+
     private 
 
     def populate(population)
-      populator = PlanetPopulator.new
-      populated_cells = populator.populate population, @planet
+      populated_cells = planet_populator.populate population, @planet
 
       populated_cells.each do |coordinate, organism|
         x = coordinate[0]
@@ -29,12 +42,10 @@ module Tordoid
     end
 
     def move_organisms
-      navigator = CellNavigator.new @planet
-
       @planet.occupied_cells.dup.map do |coordinate, organism|
         x = coordinate[0]
         y = coordinate[1]
-        navigator.move_at_random(x, y)
+        cell_navigator.move_at_random(x, y)
       end
     end
   end
