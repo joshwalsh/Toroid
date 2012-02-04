01d55591a1745bcb807c805e7597921a2ed5f578
Josh Walsh <jwalsh@designinginteractive.com>
2012-02-03 22:39:03 -0500

    Rails Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toroid::Application.config.secret_token = '272a57d27da291d97369f9d3ac90424741c23b540ba59c4a9a93c2160213f5b08690e8dad769a45e76ca453535d6c87646d01fe6bdd5558181600cdcb878b694'
