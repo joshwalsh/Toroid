2bdaf6efae4e35bc3c2e04e9c549ec26b7a69993
Josh Walsh <jwalsh@designinginteractive.com>
2012-03-04 20:14:01 -0500

    Updated persistant store

---
+++ app/services/persist_state.rb
@@ -0,0 +1,34 @@
+class PersistState
+  def initialize(current_state)
+    @current_state = current_state
+  end
+
+  def save(new_content)
+    @current_state.write new_content 
+    @current_state.rewind
+  end
+
+  def load
+    @current_state.read.tap { @current_state.rewind }
+  end
+
+  def self.save(game)
+    File.open(self.file_path, "w+") do |file|
+      new_content = YAML::dump(game)
+      new(file).save(new_content)
+    end
+  end
+
+  def self.load
+    File.open(self.file_path, "r") do |game_content|
+      p = PersistState.new game_content
+      content = p.load
+    end
+
+    content
+  end
+
+  def self.FilePath
+    "#{Rails.root}/db/#{Rails.env}.yml"
+  end
+end
---
+++ spec/services/persist_state_spec.rb
@@ -0,0 +1,21 @@
+require "lean_spec_helper"
+require "app/services/persist_state"
+
+describe PersistState do
+  # PersistGame.save game
+
+  it "writes the game file to an io object" do
+    StringIO.open("game already saved", "w+") do |game_content|
+      p = PersistState.new game_content
+      p.save("new game to save")
+      game_content.read.should eq("new game to save")
+    end
+  end
+
+  it "reads the game file from an io object" do
+    StringIO.open("existing game contents", "r") do |game_content|
+      p = PersistState.new game_content
+      p.load.should eq("existing game contents")
+    end
+  end
+end
